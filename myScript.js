206a05f21e46374d07c5f62d7cfcae597749a608
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-04-07 11:45:41 -0400

    corretto con getElementById

--- myScript.js
+++ myScript.js
@@ -1,5 +1,5 @@
-let nome = prompt("Benvenuto in questo programma dalla discutibile utilità. Qual'è il tuo primo nome?")
-let cognome = prompt("Ed il tuo cognome?")
-let colore = prompt("E il tuo colore preferito?")
+let nome = prompt("Benvenuto in questo programma dalla discutibile utilità. Qual'è il tuo primo nome?");
+let cognome = prompt("Ed il tuo cognome?");
+let colore = prompt("E il tuo colore preferito?");
 
-document.write(nome + " " + cognome + " " + colore + " 23")
+document.getElementById("testo").innerHTML = (nome + " " + cognome + " " + colore + " 23");
